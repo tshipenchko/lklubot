94558479a10ab6c6ba5e87b324af29183aec8802
Łukasz Tshipenchko <dev@zxc.sx>
2023-01-25 19:30:11 +0600

    Fix not working script

--- set_webhook.py
+++ set_webhook.py
@@ -1,18 +1,34 @@
+import asyncio
 from os import getenv
 
 from dotenv import load_dotenv
 from aiogram import Bot
 
-if __name__ == '__main__':
-    load_dotenv()
 
-    bot = Bot(
-        token=getenv("BOT_TOKEN"),
-    )
+async def set_webhook(bot: Bot, webhook_url, webhook_secret: str) -> None:
     if await bot.set_webhook(
-        url=getenv("WEBHOOK_URL"),
-        secret_token=getenv("WEBHOOK_SECRET"),
+            url=webhook_url,
+            secret_token=webhook_secret,
     ):
         print("Webhook was set successfully")
     else:
         print("Webhook was not set")
+
+
+async def main():
+    load_dotenv()
+    bot = Bot(
+        token=getenv("BOT_TOKEN"),
+    )
+    try:
+        await set_webhook(
+            bot,
+            getenv("WEBHOOK_URL"),
+            getenv("WEBHOOK_SECRET"),
+        )
+    finally:
+        await bot.session.close()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
